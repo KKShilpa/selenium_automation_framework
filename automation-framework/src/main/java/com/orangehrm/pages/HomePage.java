36ea98fbdd3d650b5aa8ed3014ac497c670e20bf
Shilpa Kamble <kambleashilpa@gmail.com>
2020-01-11 11:47:07 +0530

    Home page to commit

--- automation-framework/src/main/java/com/orangehrm/pages/HomePage.java
+++ automation-framework/src/main/java/com/orangehrm/pages/HomePage.java
@@ -1,5 +1,19 @@
 package com.orangehrm.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
 
+
+	@FindBy(id = "menu_admin_viewAdminModule" )
+	WebElement adminViewModel;
+	
+	
+	
+	public AdminPage clickOnAdminMenue()
+	{
+		adminViewModel.click();
+		return new AdminPage();
+	}
 }
